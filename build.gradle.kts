4c311d0c01218be297ee30da466453bda8d9d69f
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-06-25 10:32:06 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,19 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.detekt)
+}
+
+detekt {
+    config.setFrom("$rootDir/detekt.yml")
+    source.setFrom("$rootDir/app/src/main/java")
+    buildUponDefaultConfig = true
+}
+
+subprojects {
+    afterEvaluate {
+        tasks.matching { it.name.startsWith("compile") && it.name.endsWith("Kotlin") }.configureEach {
+            dependsOn(rootProject.tasks.named("detekt"))
+        }
+    }
 }
