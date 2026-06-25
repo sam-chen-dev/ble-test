06024923d43e762f195ca59b7b40c72a00b84684
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-06-24 23:40:34 -0400

    Update BleManager.kt

--- app/src/main/java/com/samchendev/blecompose/ble/BleManager.kt
+++ app/src/main/java/com/samchendev/blecompose/ble/BleManager.kt
@@ -7,9 +7,12 @@ import kotlinx.coroutines.flow.update
 
 class BleManager(private val bleController: BleController) {
     private val _scannedDevices = MutableStateFlow<List<BluetoothDevice>>(emptyList())
+    private val _connectionState = MutableStateFlow(ConnectionState.DISCONNECTED)
 
     val scannedDevices = _scannedDevices.asStateFlow()
+    val connectionState = _connectionState.asStateFlow()
 
+    /*Scan*/
     fun startScan() = bleController.startScan { device -> updateScannedDevices(device) }
 
     fun stopScan() = bleController.stopScan()
@@ -19,4 +22,21 @@ class BleManager(private val bleController: BleController) {
 
         _scannedDevices.update { it + newDevice }
     }
+
+    /*Connect*/
+    fun connect(address: String) {
+        updateConnectionState(ConnectionState.CONNECTING)
+
+        bleController.connect(address) { state -> updateConnectionState(state) }
+    }
+
+    fun disconnect() = bleController.disconnect()
+
+    fun closeGatt() {
+        bleController.closeGatt()
+
+        updateConnectionState(ConnectionState.DISCONNECTED)
+    }
+
+    private fun updateConnectionState(newState: ConnectionState) = _connectionState.update { newState }
 }
