676ae5dcaaad262939aff7bfd60cce355aa28f93
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-06-24 23:40:08 -0400

    Update BleControllerImpl.kt

--- app/src/main/java/com/samchendev/blecompose/ble/BleControllerImpl.kt
+++ app/src/main/java/com/samchendev/blecompose/ble/BleControllerImpl.kt
@@ -2,7 +2,10 @@ package com.samchendev.blecompose.ble
 
 import android.Manifest
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.Context
@@ -16,6 +19,8 @@ class BleControllerImpl(context: Context) : BleController {
     private val applicationContext = context.applicationContext
     private val bleManager = applicationContext.getSystemService(BluetoothManager::class.java)
     private val bleScanner = bleManager.adapter.bluetoothLeScanner
+
+    /*Scan*/
     private var scanCallback: ScanCallback? = null
 
     override fun startScan(onDeviceFound: (BluetoothDevice) -> Unit) {
@@ -34,6 +39,42 @@ class BleControllerImpl(context: Context) : BleController {
             if (result.device.address != TARGET_ADDRESS) return
 
             onDeviceFound(result.device)
+        }
+    }
+
+    /*Connect*/
+    private var gatt: BluetoothGatt? = null
+    private var gattCallback: BluetoothGattCallback? = null
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
+    override fun connect(address: String, onConnectionStateChanged: (ConnectionState) -> Unit) {
+        val device = bleManager.adapter.getRemoteDevice(address)
+
+        gattCallback = createGattCallback(onConnectionStateChanged)
+        gatt = device.connectGatt(applicationContext, false, gattCallback)
+    }
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
+    override fun disconnect() {
+        gatt?.disconnect()
+    }
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
+    override fun closeGatt() {
+        gatt?.disconnect()
+        gatt?.close()
+        gatt = null
+    }
+
+    private fun createGattCallback(
+        onConnectionStateChanged: (ConnectionState) -> Unit
+    ) = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            when (newState) {
+                BluetoothProfile.STATE_CONNECTING -> onConnectionStateChanged(ConnectionState.CONNECTING)
+                BluetoothProfile.STATE_CONNECTED -> onConnectionStateChanged(ConnectionState.CONNECTED)
+                else -> onConnectionStateChanged(ConnectionState.DISCONNECTED)
+            }
         }
     }
 }
