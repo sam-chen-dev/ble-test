99f7f72b2e367f6158362247fe3eb4d3ed485d08
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-07-16 16:17:56 -0400

    Update BleConnectUiState.kt

--- app/src/main/java/com/samchendev/blecompose/bleConnect/BleConnectUiState.kt
+++ app/src/main/java/com/samchendev/blecompose/bleConnect/BleConnectUiState.kt
@@ -2,12 +2,15 @@ package com.samchendev.blecompose.bleConnect
 
 import com.samchendev.blecompose.ble.ConnectionState
 import com.samchendev.blecompose.ble.GattService
+import java.util.UUID
 
 data class BleConnectUiState(
     val deviceName: String?,
     val deviceAddress: String,
     val connectionState: ConnectionState,
     val services: List<GattService>,
+    val characteristicValues: Map<UUID, ByteArray>,
     val onConnectTrigger: () -> Unit,
-    val onDisconnectTrigger: () -> Unit
+    val onDisconnectTrigger: () -> Unit,
+    val onCharacteristicClick: (serviceUuid: UUID, characteristicUuid: UUID) -> Unit
 )
