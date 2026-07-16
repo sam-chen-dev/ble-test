49e3db1d30d7f6094a26d1342e5617b1630efadf
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-07-16 15:29:12 -0400

    Update BleController.kt

--- app/src/main/java/com/samchendev/blecompose/ble/BleController.kt
+++ app/src/main/java/com/samchendev/blecompose/ble/BleController.kt
@@ -1,6 +1,7 @@
 package com.samchendev.blecompose.ble
 
 import android.bluetooth.BluetoothDevice
+import java.util.UUID
 
 interface BleController {
     fun startScan(onDeviceFound: (BluetoothDevice) -> Unit)
@@ -10,10 +11,13 @@ interface BleController {
     fun connect(
         address: String,
         onConnectionStateChanged: (ConnectionState) -> Unit,
-        onServicesDiscovered: (List<GattService>) -> Unit
+        onServicesDiscovered: (List<GattService>) -> Unit,
+        onCharacteristicRead: (UUID, ByteArray) -> Unit
     )
 
     fun disconnect()
 
     fun closeGatt()
+
+    fun readCharacteristic(serviceUuid: UUID, characteristicUuid: UUID)
 }
