2fe4435ec5f91faef7723975938716693f494641
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-07-16 23:24:51 -0400

    Update GattCharacteristic.kt

--- app/src/main/java/com/samchendev/blecompose/ble/GattCharacteristic.kt
+++ app/src/main/java/com/samchendev/blecompose/ble/GattCharacteristic.kt
@@ -8,6 +8,10 @@ data class GattCharacteristic(
 ) {
     fun getFormattedValue(value: ByteArray) = when (uuid.toBleId()) {
         BleFeature.DEVICE_NAME.id -> value.toText()
+        BleFeature.MANUFACTURER_NAME_STRING.id -> value.toText()
+        BleFeature.MODEL_NUMBER_STRING.id -> value.toText()
+        BleFeature.HARDWARE_REVISION_STRING.id -> value.toText()
+        BleFeature.FIRMWARE_REVISION_STRING.id -> value.toText()
         BleFeature.CELSIUS.id -> "${value.toFloat()}°C"
         else -> value.toHexString()
     }
